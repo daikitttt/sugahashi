85a7689b59c259579a87b5d08d4a543704871331
taro-y <taro-y@example.com>
2024-05-24 16:57:08 +0900

    Form.java confirm.html commit1

--- src/main/java/com/example/demo/form/Form.java
+++ src/main/java/com/example/demo/form/Form.java
@@ -1,5 +1,20 @@
 package com.example.demo.form;
 
 public class Form {
-
+	
+	private String name1;
+	private String place;
+	public Form() {}
+	public String getName1() {
+		return name1;
+	}
+	public void setName1(String name1) {
+		this.name1 = name1;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
 }
