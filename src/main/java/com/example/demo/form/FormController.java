ad7f0c6455c7ea67d1405909176f78e02afc9771
FJ2-0183\Owner <gayano1229@gmail.com>
2024-05-24 17:01:01 +0900

    Second commit

--- src/main/java/com/example/demo/form/FormController.java
+++ src/main/java/com/example/demo/form/FormController.java
@@ -9,7 +9,7 @@ public class FormController {
 	
 	@RequestMapping("/index")
 	public String index(Model model, Form form) {
-		model.addAttribute("message", "Hello World2"); 
+		model.addAttribute("message", "ホーム"); 
 		return "index";
 	}
 	@RequestMapping("/input")
@@ -21,6 +21,23 @@ public class FormController {
 	public String confirm(Model model, Form form) {
 		model.addAttribute("message", "Hello World2"); 
 		return "form/confirm";
+	}
+	@RequestMapping("/top")
+	public String top(Model model, Form form) {
+		model.addAttribute("title", "上段"); 
+		return "form/top";
+	}
+
+	@RequestMapping("/middle")
+	public String middle(Model model, Form form) {
+		model.addAttribute("title", "中段"); 
+		return "form/middle";
+	}
+
+	@RequestMapping("/bottom")
+	public String bottom(Model model, Form form) {
+		model.addAttribute("title", "下段"); 
+		return "form/bottom";
 	}
 
 
